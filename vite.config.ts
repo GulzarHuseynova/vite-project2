453e49c92eab1c99d4a978abeb29be608b24422e
Gulzar <gulzarhuseynovaa@gmail.com>
2026-06-10 10:36:30 +0400

    fix: local proxy added to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
+  server: {
+    proxy: {
+      '/intern-api': {
+        target: 'http://161.97.154.119',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
